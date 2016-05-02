d06f3134e77e8bcdbf00b8363cdeb25ce9d9965c
Nick <koxlienn@gmail.com>
2016-05-02 18:59:53 -0500

    starting to break up main to get ready for Tiff's code

--- drivers/driver.c
+++ drivers/driver.c
@@ -6,65 +6,19 @@
        #include <string.h>
        #include <sys/socket.h>
        #include <netdb.h>
+       #include "driver.h"
        #include "ArmTranslator.h"
        #define BUF_SIZE 4
 
        int
        main(int argc, char *argv[])
        {
-           struct addrinfo hints;
-           struct addrinfo *result, *rp;
-           int sfd, s, n;
-           struct sockaddr_storage peer_addr;
-           socklen_t peer_addr_len;
-           ssize_t nread;
-           char buf[BUF_SIZE];
-
-           memset(&hints, 0, sizeof(struct addrinfo));
-           hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-           hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
-           hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
-           hints.ai_protocol = 0;          /* Any protocol */
-           hints.ai_canonname = NULL;
-           hints.ai_addr = NULL;
-           hints.ai_next = NULL;
-
-           s = getaddrinfo(NULL, "22", &hints, &result);
-           if (s != 0) {
-               fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-               exit(EXIT_FAILURE);
-           }
-
-           /* getaddrinfo() returns a list of address structures.
-              Try each address until we successfully bind(2).
-              If socket(2) (or bind(2)) fails, we (close the socket
-              and) try the next address. */
-
-           for (rp = result; rp != NULL; rp = rp->ai_next) {
-               sfd = socket(rp->ai_family, rp->ai_socktype,
-                       rp->ai_protocol);
-               if (sfd == -1)
-                   continue;
-
-               if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0){
-		   printf("Bound!!\n");
-		   break;                  /* Success */
-	       }
-               close(sfd);
-           }
-
-           if (rp == NULL) {               /* No address succeeded */
-               fprintf(stderr, "Could not bind\n");
-               exit(EXIT_FAILURE);
-           }
-           freeaddrinfo(result);           /* No longer needed */
-
-           /* Read datagrams and echo them back to sender */
-
-           for (;;) {
+	   socketBind();
+           
+	   for (;;) {
 		printf("Reading loop\n");
                peer_addr_len = sizeof(struct sockaddr_storage);
-               nread = recvfrom(sfd, buf, BUF_SIZE, 0,
+               nread = recvfrom(sfd, xyzBuf, BUF_SIZE, 0,
                        (struct sockaddr *) &peer_addr, &peer_addr_len);
 
 	        
@@ -73,14 +27,55 @@
 	       		continue;               /* Ignore failed request */
 		}
 		
-		if( buf[2]   == 0x00 )	
-			printf("x position: %hi\n ", (short)(buf[0] | (buf[1] << 8)));
-	     	else if( buf[2]  == 0x0001 )	
-			printf("y position: %hi\n ", (short)(buf[0] | buf[1] << 8));
+		if( xyzBuf[2]   == 0x00 )	
+			printf("x position: %hi\n ", (short)(xyzBuf[0] | (xyzBuf[1] << 8)));
+	     	else if( xyzBuf[2]  == 0x0001 )	
+			printf("y position: %hi\n ", (short)(xyzBuf[0] | (xyzBuf[1] << 8)));
 		
-		else if( buf[2]  == 0x0002 )	
-			printf("z position: %hi\n ", (short)(buf[0] | buf[1] << 8));
+		else if( xyzBuf[2]  == 0x0002 )	
+			printf("z position: %hi\n ", (short)(xyzBuf[0] | (xyzBuf[1] << 8)));
 		else
 			printf("Received garbage...\n");
            }
        }
+
+int socketBind(){
+	
+	memset(&hints, 0, sizeof(struct addrinfo));
+        hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+        hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+        hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+        hints.ai_protocol = 0;          /* Any protocol */
+        hints.ai_canonname = NULL;
+        hints.ai_addr = NULL;
+        hints.ai_next = NULL;
+
+        s = getaddrinfo(NULL, "22", &hints, &result);
+           
+	if (s != 0) {
+        	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+        }
+
+
+        for (rp = result; rp != NULL; rp = rp->ai_next) {
+            sfd = socket(rp->ai_family, rp->ai_socktype,
+                    rp->ai_protocol);
+            if (sfd == -1)
+                continue;
+
+            if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0){
+		printf("Bound!!\n");
+		break;                  /* Success */
+	    }
+               close(sfd);
+        }
+
+        if (rp == NULL) {               /* No address succeeded */
+            fprintf(stderr, "Could not bind\n");
+            exit(EXIT_FAILURE);
+        }
+        freeaddrinfo(result);           /* No longer needed */
+
+	return 0;
+}
---
+++ include/driver.h
@@ -0,0 +1,37 @@
+#ifndef DRIVER_H__
+#define DRIVER_H_
+
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include "ArmTranslator.h"
+#define BUF_SIZE 4
+
+
+struct addrinfo hints;
+struct addrinfo *result, *rp;
+int sfd, s, n;
+struct sockaddr_storage peer_addr;
+socklen_t peer_addr_len;
+ssize_t nread;
+char xyzBuf[BUF_SIZE];
+
+
+
+/**********************************************
+* Method creates socket and binds it to port 22
+* returns 0 on success
+**********************************************/
+int socketBind();
+	
+
+
+
+
+
+#endif //DRIVER_H___
